01f0561bfca93d40dcb5ed2f7b8f234eed4ae18e
KEHFAN <33714955+KEHFAN@users.noreply.github.com>
2020-06-16 22:37:05 +0800

    Add files via upload

---
+++ learn/spring/spring5/src_05_内部bean注入对象类型属性/com/fkh/spring5/Dept.java
@@ -0,0 +1,16 @@
+package com.fkh.spring5;
+
+public class Dept {
+    private String dname;
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "dname='" + dname + '\'' +
+                '}';
+    }
+}
